494b56a2f0605b98b6d4e87980f70f8df1ed0904
Roadagain <ringoh72@gmail.com>
2018-04-30 23:46:36 +0900

    関数化してmain実行のときだけ取得結果を保存

--- fetcher.py
+++ fetcher.py
@@ -1,23 +1,25 @@
 import tweepy
 import json
 
-tokens = json.load(open('tokens.json'))
+def fetch_nijisanji_tweets(tokens):
+    auth = tweepy.OAuthHandler(tokens['consumer_key'], tokens['consumer_secret'])
+    auth.set_access_token(tokens['access_token'], tokens['access_token_secret'])
+    params = {
+            'screen_name': 'nijisanji_app',
+            'exclude_replies': True,
+            'trim_user': True,
+            }
 
-auth = tweepy.OAuthHandler(tokens['consumer_key'], tokens['consumer_secret'])
-auth.set_access_token(tokens['access_token'], tokens['access_token_secret'])
-params = {
-        'screen_name': 'nijisanji_app',
-        'exclude_replies': True,
-        'trim_user': True,
-        }
+    api = tweepy.API(auth)
+    try:
+        data = api.user_timeline(**params)
+        return list(map(lambda x: x.text, data))
+    except tweepy.error.TweepError as e:
+        print(e)
+        exit(-1)
 
-api = tweepy.API(auth)
-try:
-    data = api.user_timeline(**params)
-except tweepy.error.TweepError as e:
-    print(e)
-    exit(-1)
-tweets = list(map(lambda x: x.text, data))
-
-with open('result.txt', 'w') as result:
-    result.write(json.dumps({'tweets': tweets}, ensure_ascii=False))
+if __name__ == '__main__':
+    tokens = json.load(open('config.json'))['tokens']
+    tweets = fetch_tweets(tokens)
+    with open('result.txt', 'w') as result:
+        result.write(json.dumps({'tweets': tweets}, ensure_ascii=False))
