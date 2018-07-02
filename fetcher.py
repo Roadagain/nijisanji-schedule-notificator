5d435a28f0102fbe59e1c139be6619c4cf91cf30
Roadagain <ringoh72@gmail.com>
2018-07-02 23:15:42 +0900

    ツイートにフィルターかける部分は別モジュールに分離

--- fetcher.py
+++ fetcher.py
@@ -1,17 +1,8 @@
 import tweepy
 import json
-from datetime import date
-from pytz import timezone
 from itertools import chain
 from functools import reduce
-
-def diet_tweet_object(tweet):
-    return {
-            'text': tweet.full_text,
-            'id': tweet.id,
-            'created_at': tweet.created_at,
-            'reply_to': tweet.in_reply_to_status_id
-            }
+import tweetfilter
 
 def fetch_tweets(screen_name, tokens):
     auth = tweepy.OAuthHandler(tokens['consumer_key'], tokens['consumer_secret'])
@@ -26,7 +17,7 @@ def fetch_tweets(screen_name, tokens):
     api = tweepy.API(auth)
     try:
         data = api.user_timeline(**params)[::-1]
-        return map(diet_tweet_object, data)
+        return map(tweetfilter.diet_tweet_object, data)
     except tweepy.error.TweepError as e:
         print(e)
         exit(-1)
@@ -36,35 +27,10 @@ def fetch_nijisanji_tweets(tokens):
     tweets = [fetch_tweets(i, tokens) for i in accounts]
     return reduce(chain, tweets)
 
-def search_schedule(tweets):
-    def has_schedule(tweet):
-        today = date.today()
-        today_strs = [
-                f'{today.month}/{today.day}',
-                f'{today.month}月{today.day}日',
-                ]
-        created_at = tweet['created_at'].astimezone(timezone('Asia/Tokyo')).date()
-        if today == created_at:
-            today_strs.append('本日')
-        text = tweet['text'].replace('\n', ' ')
-        return any([s in text for s in today_strs])
-
-    indexed_tweets = {}
-    scheduled = []
-    for tweet in tweets:
-        indexed_tweets[tweet['id']] = tweet
-        reply_to = tweet['reply_to']
-        if has_schedule(tweet):
-            scheduled.append(tweet)
-        elif reply_to in indexed_tweets and has_schedule(indexed_tweets[reply_to]):
-            # TODO: 再帰的にリプライを遡る
-            scheduled.append(tweet)
-    return scheduled
-
 if __name__ == '__main__':
     tokens = json.load(open('config.json'))['tokens']
     tweets = list(fetch_nijisanji_tweets(tokens))
-    schedule = search_schedule(tweets)
+    schedule = tweetfilter.search_schedule(tweets)
     print(schedule)
     with open('result.json', 'w') as result:
         result.write(json.dumps({'tweets': tweets}, ensure_ascii=False))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import fetcher
+import tweetfilter
 import poster
 import os
 
@@ -17,7 +18,7 @@ def main():
         old_schedule_id = 0
 
     tweets = fetcher.fetch_nijisanji_tweets(tokens)
-    schedule_tweets = [t for t in fetcher.search_schedule(tweets) if t['id'] > old_schedule_id]
+    schedule_tweets = [t for t in tweetfilter.search_schedule(tweets) if t['id'] > old_schedule_id]
     if schedule_tweets == []:
         schedule_id = 0
     else:
